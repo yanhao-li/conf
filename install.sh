e4d2ed05fed64e02bef0a0659b4b41800f31e700
Yanhao Li <yanhaoli@Macbook.local>
2021-01-02 12:34:49 -0500

    fix: pwd in install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # zsh
-ln -sf "$pwd/zsh/zshrc" ~/.zshrc
+ln -sf "$(pwd)/zsh/zshrc" ~/.zshrc
 
 # vim
-ln -sf "$pwd/vim/vimrc" ~/.vimrc
+ln -sf "$(pwd)/vim/vimrc" ~/.vimrc
 
 # git
-ln -sf "$pwd/git/.gitconfig" ~/.gitconfig
-ln -sf "$pwd/git/.gitignore_global" ~/.gitignore_global
-ln -sf "$pwd/git/.gitattributes" ~/.gitattributes
+ln -sf "$(pwd)/git/.gitconfig" ~/.gitconfig
+ln -sf "$(pwd)/git/.gitignore_global" ~/.gitignore_global
+ln -sf "$(pwd)/git/.gitattributes" ~/.gitattributes
